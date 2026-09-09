3992edc758a89a879798f31a834e10f7c5b53345
ali kamal <ali123kamal12@gmail.com>
2026-09-09 12:59:55 +0300

    6th answer

--- Task-03/Program.cs
+++ Task-03/Program.cs
@@ -40,6 +40,7 @@ namespace Task_03
             string title = "Clean Code";
             int pages = 464;
             Console.WriteLine("Book: " + title + "," + " " + "Pages: " + pages);
+            Console.WriteLine($"Book: {title}, Pages: {pages}");
             #endregion
         }
     }
